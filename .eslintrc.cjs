775a600d573e8c5bab45599df9fc908640da82c8
Sahar Javadi <sahar.javadi.h@gmail.com>
2025-05-02 21:11:22 +0330

    edit some files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,16 +2,17 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
   rules: {
-  "react/prop-types": "off"
-}
-  
-}
+    "react/prop-types": "off",
+    "no-unused-vars": "off",
+    "no-undef": "off",
+  },
+};
